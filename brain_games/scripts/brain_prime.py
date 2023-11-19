fba935b808028af0e1a075cdea6038191b02ecb2
CoinerLo <coiner4677@gmail.com>
2023-11-19 18:51:32 +0300

    add prime game

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+from brain_games import cli
+from random import randrange
+from brain_games import utils
+import prompt
+
+
+def question():
+    number = randrange(1, 1000001)
+
+    k = 0
+    for i in range(2, number // 2+1):
+      if (number % i == 0):
+          k = k+1
+
+    correct_answer = 'yes' if k <= 0 else 'no'
+    print(f'Question: {number}')
+    answer = prompt.string('Your answer: ')
+    return (correct_answer, answer)
+
+
+def main():
+    name = cli.welcome_user()
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    count_answers = 0
+    while count_answers < utils.number_of_rounds:
+        (correct_answer, answer) = question()
+        if answer.lower() == correct_answer:
+            count_answers += 1
+            print('Correct!')
+        else:
+            print(f'\'{answer}\' is wrong answer ;(. Correct answer was \'{correct_answer}\'.')
+            print(f'Let\'s try again, {name}!')
+            return None
+    print(f'Congratulations, {name}!')
+
+
+if __name__ == '__main__':
+    main()
